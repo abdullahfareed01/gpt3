619d4748c0cf0fcd8788e4c3ff6fd4aaa088ef17
Abdullah Fareed <your-email@example.com>
2025-10-13 09:38:55 +0500

    Ensure react-icons dependency is pushed and update Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // 👈 VERY IMPORTANT
-})
+  build: {
+    rollupOptions: {
+      external: ["react-icons/ri"],
+    },
+  },
+});
