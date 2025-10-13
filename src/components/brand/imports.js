71374b9712d7a9d1041c6eacf4b3613a0c553c28
Abdullah Fareed <your-email@example.com>
2025-10-13 09:48:29 +0500

    import issue resolved

--- src/components/brand/imports.js
+++ src/components/brand/imports.js
@@ -2,7 +2,7 @@ import google from '../../assets/google.png';
 import slack from '../../assets/slack.png';
 import atlassian from '../../assets/atlassian.png';
 import dropbox from '../../assets/dropbox.png';
-import Shopify from '../../assets/Shopify.png';
+import Shopify from '../../assets/shopify.png';
 
 
 
